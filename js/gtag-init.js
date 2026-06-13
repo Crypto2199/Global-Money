68f96baa27e6ab568c164a7f3f14612e4a73fa1e
Crypto21 <cryptodz2199@gmail.com>
2026-06-13 22:04:29 +0100

    Add files via upload

---
+++ js/gtag-init.js
@@ -0,0 +1,5 @@
+// Google Analytics initialization — loaded after consent check
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-5QWDCB9350');
